e685ef0d1176ceedc83d5dc415257d3363af842c
K Lauer <kenneth.lauer@gmail.com>
2018-11-02 07:39:44 -0700

    Run dev env script from CI_TOP

--- ci-scripts/run-epics-iocs-on-tmux.sh
+++ ci-scripts/run-epics-iocs-on-tmux.sh
@@ -11,24 +11,24 @@ tmux new-session -d -s ${EPICS_TMUX_SESSION} /bin/bash
 tmux set remain-on-exit on
 
 echo "Starting the pyepics test IOC..."
-tmux new-window -n 'pyepics-test_ioc' -c "${TRAVIS_BUILD_DIR}" \
+tmux new-window -n 'pyepics-test_ioc' -c "${CI_TOP}" \
     "source setup_local_dev_env.sh; \
     cd "${PYEPICS_IOC}/iocBoot/iocTestioc" && \
     ${PYEPICS_IOC}/bin/${EPICS_HOST_ARCH}/testioc ./st.cmd"
 
 echo "Starting the motorsim IOC..."
-tmux new-window -n 'motorsim_ioc' -c "${TRAVIS_BUILD_DIR}"  \
+tmux new-window -n 'motorsim_ioc' -c "${CI_TOP}"  \
     "source setup_local_dev_env.sh; \
     cd "${MOTORSIM_IOC}/iocBoot/ioclocalhost" && \
     ${MOTORSIM_IOC}/bin/${EPICS_HOST_ARCH}/mtrSim ./st.cmd"
 
 echo "Starting the ADSim IOC..."
-tmux new-window -n 'adsim_ioc' -c "${TRAVIS_BUILD_DIR}"  \
+tmux new-window -n 'adsim_ioc' -c "${CI_TOP}"  \
     "source setup_local_dev_env.sh; \
     cd "${ADSIM_IOC}/iocBoot/iocSimDetector" && \
     ${ADSIM_IOC}/bin/${EPICS_HOST_ARCH}/simDetectorApp ./st.cmd"
 
-timeout --kill-after=30s bash $CI_SCRIPTS/ensure-iocs-are-running.sh
+timeout --kill-after=25s 30s $CI_SCRIPTS/ensure-iocs-are-running.sh
 
 echo "All IOCs are running in tmux!"
 
