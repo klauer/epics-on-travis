70f60eda00cb20d5dc6a2d53358b6829d29b928c
K Lauer <kenneth.lauer@gmail.com>
2018-11-22 15:04:43 -0800

    host arch

--- azure-linux.sh
+++ azure-linux.sh
@@ -18,7 +18,7 @@ bash "${CI_SCRIPTS}/install-epics-base.sh"
 # bash "${CI_SCRIPTS}/install-epics-iocs.sh"
 
 export VERSION_TAG=${BASE_VER}_pva${PVA}_areadetector${AREADETECTOR_VER}_motor${MOTOR_VER}
-export FILE_TO_UPLOAD="${TRAVIS_BUILD_DIR}/epics-ci-${TRAVIS_TAG}_${VERSION_TAG}.tar.bz2"
+export FILE_TO_UPLOAD="${HOME}/epics-ci-${EPICS_HOST_ARCH}-${TRAVIS_TAG}_${VERSION_TAG}.tar.bz2"
 echo $FILE_TO_UPLOAD
 
 # Reduce the deployed package size a bit:
@@ -29,7 +29,7 @@ rm -rf $BUSY_PATH/lib      $BUSY_PATH/bin
 rm -rf $ASYN_PATH/lib      $ASYN_PATH/bin
 rm -rf $CALC_PATH/lib      $CALC_PATH/bin
 rm -rf $MOTOR_PATH/lib     $MOTOR_PATH/bin
-find $AREA_DETECTOR_PATH -type f -name "*.a" -delete
-find $AREA_DETECTOR_PATH -type f -name "*.so" -delete
-find $AREA_DETECTOR_PATH -type f -name "*.dylib" -delete
+# find $AREA_DETECTOR_PATH -type f -name "*.a" -delete
+# find $AREA_DETECTOR_PATH -type f -name "*.so" -delete
+# find $AREA_DETECTOR_PATH -type f -name "*.dylib" -delete
 # tar cfj ${FILE_TO_UPLOAD} ${EPICS_ROOT} --exclude=${EPICS_ROOT}/modules
