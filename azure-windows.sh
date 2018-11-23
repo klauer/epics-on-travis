b9da2bef1792a31855b8b8947e6318e1f7f5c180
K Lauer <kenneth.lauer@gmail.com>
2018-11-23 08:53:05 -0800

    windows

--- azure-windows.sh
+++ azure-windows.sh
@@ -38,14 +38,14 @@ env |grep _VER
 source "${CI_SCRIPTS}/epics-config.sh"
 
 bash "${CI_SCRIPTS}/install-epics-base.sh"
-# if [[ ! -z "${PVA}" ]]; then
-#    bash "${CI_SCRIPTS}/install-epics-v4.sh";
-# fi
-# bash "${CI_SCRIPTS}/install-epics-modules.sh"
-# if [[ ! -z "${AREADETECTOR_VER}" ]]; then
-#    bash "${CI_SCRIPTS}/install-epics-areadetector.sh";
-# fi
-# bash "${CI_SCRIPTS}/install-epics-iocs.sh"
+if [[ ! -z "${PVA}" ]]; then
+   bash "${CI_SCRIPTS}/install-epics-v4.sh";
+fi
+bash "${CI_SCRIPTS}/install-epics-modules.sh"
+if [[ ! -z "${AREADETECTOR_VER}" ]]; then
+   bash "${CI_SCRIPTS}/install-epics-areadetector.sh";
+fi
+bash "${CI_SCRIPTS}/install-epics-iocs.sh"
 
 export GIT_TAG=$(git describe --tag)
 export VERSION_TAG=${BASE_VER}_pva${PVA}_areadetector${AREADETECTOR_VER}_motor${MOTOR_VER}
