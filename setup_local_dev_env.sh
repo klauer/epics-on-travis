f92eb20becc806e8948e4f4270c837437f4424e7
K Lauer <klauer@slac.stanford.edu>
2019-02-15 16:25:44 -0800

    Remove confusing version settings

--- setup_local_dev_env.sh
+++ setup_local_dev_env.sh
@@ -18,28 +18,10 @@ echo "EPICS_HOST_ARCH=$EPICS_HOST_ARCH"
 export EPICS_CA_ADDR_LIST=127.255.255.255
 export EPICS_CA_AUTO_ADDR_LIST=NO
 export EPICS_CA_MAX_ARRAY_BYTES=10000000
-# example build matrix variables
-export BASE_VER=R7.0.1.1
-# export BASE_VER=R3.15.5
-# export BASE_VER=R3.14.12.6
-export BUSY_VER=1-6-1
-export SEQ_VER=2.2.5
-export ASYN_VER=4-32
-export CALC_VER=3-7
-export MOTOR_VER=6-10
-export AUTOSAVE_VER=5-9
-export SSCAN_VER=2-11-1
-export AREADETECTOR_VER=3-2
-export STATIC_BUILD=YES
 
-if [[ "$BASE_VER" == R3.14* ]]; then
-    export PVA=
-    export WITH_PVA=NO
-elif [[ "$BASE_VER" == R7* ]]; then
-    export PVA=
+if [[ -z "$PVA" ]]; then
     export WITH_PVA=NO
 else
-    export PVA=4.7.0
     export WITH_PVA=YES
 fi
 
