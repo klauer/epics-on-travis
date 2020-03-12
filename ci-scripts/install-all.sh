7b047cfa79afb607e5735d29d1a3e6b262bb7411
Thomas A Caswell <tcaswell@bnl.gov>
2020-03-12 12:21:08 -0400

    BLD: set extra include paths to make tirpc work

--- ci-scripts/install-all.sh
+++ ci-scripts/install-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e -x
 
+# fix rpc/rpc.h related issues
+export C_INCLUDE_PATH=/usr/include/tirpc/
+
 ./ci-scripts/install-epics-base.sh
 ./ci-scripts/install-epics-modules.sh
 ./ci-scripts/install-epics-areadetector.sh
